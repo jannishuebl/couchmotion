1028c0921452f05675d19dc96ff96e750713868e
Jannis Hübl <jannis.huebl@web.de>
2015-02-08 12:58:01 +0100

    add double database for testing purposes

---
+++ lib/double/couch_db/view/double_view/double_view.rb
@@ -0,0 +1,57 @@
+class CouchDB
+  class View
+    # TODO: Reduce process block
+    class DoubleView
+
+      def initialize(double_database)
+        @double_database = double_database
+        @reduce = nil
+      end
+
+      def map(&block)
+        @map = block
+        save
+      end
+
+      def reduce(&block)
+        @reduce = block
+        save
+      end
+
+      def version(version)
+        @version = version
+        save
+      end
+
+      def save
+        if @map && @version
+          return true
+        end
+        false
+      end
+
+      def create_query
+        @query_documents = []
+        @query_values = []
+        @query_keys = []
+
+        @double_database.documents.each do | key, document |
+          @actual_document = document
+          @map.call document, self
+        end
+
+        if @reduce
+          reduce_value = @reduce.call @query_keys, @query_values, false
+        end
+
+        CouchDB::Query::DoubleQuery.new @query_documents, @query_keys, @query_values, reduce_value
+      end
+
+      def emit(key, value)
+        @query_documents << {key: key, document: @actual_document}
+        @query_values    << value
+        @query_keys      << key
+      end
+    end
+  end
+end
---
+++ lib/double/double_db.rb
@@ -0,0 +1,10 @@
+
+require_relative 'couch_db/couch_db'
+require_relative 'couch_db/double_couch_db/double_couch_db'
+require_relative 'couch_db/document/double_document/double_document'
+require_relative 'couch_db/view/double_view/double_view'
+require_relative 'couch_db/query/double_query/double_query'
+require_relative 'couch_db/enumerator/double_enumerator/double_enumerator'
+
+require_relative '../general/couch_db/couch_db_facade'
+require_relative '../general/couch_db/couch_db_impl'
