9bea6824a8766d8ae2576b3caf4049c15de8c6bd
Jannis Hübl <jannis.huebl@web.de>
2015-02-09 11:55:04 +0100

    add some rspec 3.0 helpers(hack)

--- specs/ios/helpers/ios_helper.rb
+++ specs/ios/helpers/ios_helper.rb
@@ -1,20 +1,45 @@
+# Hack to make rubymotion testing more rspec 3.0 like
 class Bacon::Context
-  def expect(actual)
-    @actual = actual
-    self
-  end
 
-  def be_kind_of(clazz)
-    @actual.should.kind_of clazz
+  def expect(actual)
+    @actual = actual.should
     self
   end
 
   def to(*args)
+    @be.call
+    @be = nil
+    @actual = nil
     self
   end
 
-  def be(expected)
-    @actual.should == expected
+  def not_to(*args)
+    @actual = @actual.not
+    to args
     self
   end
+
+  def be_kind_of(clazz)
+    @be = lambda {
+      @actual.should.kind_of clazz
+    }
+    self
+  end
+
+
+  def be(expected)
+    @be = lambda {
+      @actual.identical_to expected
+    }
+    self
+  end
+
+
+  def eq(expected)
+    @be = lambda {
+      @actual.equal expected
+    }
+    self
+  end
+
 end
